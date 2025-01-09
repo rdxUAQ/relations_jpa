b93a9948485eadaa70ab12c84d33dd1de06b1603
rdxUAQ <gmorales37@alumnos.uaq.mx>
2025-01-09 11:44:37 -0600

    first commit

---
+++ src/main/java/com/sb/jparelations/relations_jpa/RelationsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sb.jparelations.relations_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/jparelations/relations_jpa/RelationsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.jparelations.relations_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
